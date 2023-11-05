e1a8b185bcf7f6f0532f6f17aa11fda5cea90621
Neha <nehad2000@gmail.com>
2023-11-04 22:24:55 -0400

    working through data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-
+import {puppyList} from './data.js'
+import {useState} from 'react'
 function App() {
-
+  const{puppies, setPuppies} = useState(puppyList)
+console.log(puppyList)
   return (
     <>
       <div>
